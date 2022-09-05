d07ba07d9fdfa7fff868bf83d273245606ba11c7
kharkevich-yuliya <99814691+kharkevich-yuliya@users.noreply.github.com>
2022-09-05 19:35:46 +0300

    3 files modified

--- index.js
+++ index.js
@@ -9,18 +9,33 @@ const formEl = document.querySelector('#form');
 
 formEl.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(e.target.elements);
-  console.log(e.target);
-  const elements = [...e.target.elements];
-  elements.pop();
+  const inputs = ['loan', 'rate', 'loanLength'];
+  const elements = [];
 
-  const principal = elements[0].value;
+  [...e.target.elements].forEach (el => {
+    if (inputs.includes(el.id)) {
+      elements.push(el)
+    } 
+  })
+
+  const principal = parseInt(elements[0].value, 10);
   const rate = elements[1].value;
   const loanLength = elements[2].value;
 
-  console.log({
-    principal,
-    rate,
-    loanLength,
-  });
+  const interestRateMonths = (parseFloat(rate) * 0.01) / 12;
+  const loanLengthMonths = parseInt(loanLength) * 12;
+
+  const top =
+    interestRateMonths * Math.pow(1 + interestRateMonths, loanLengthMonths);
+  const bottom = Math.pow(1 + interestRateMonths, loanLengthMonths) - 1;
+
+  const morgagePayment = parseInt(principal * (top / bottom));
+
+  const infoTextEl = document.querySelector('.info-text');
+
+  infoTextEl.textContent = `Your monthly morgage payments will be $${morgagePayment}`;
+
+  document.getElementById('loan').value = '';
+  document.querySelector('#loanLength').value = '';
+  document.querySelector('#rate').value = '';
 });
